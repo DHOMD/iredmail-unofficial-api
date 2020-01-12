cd5af8146bf92be039e0ed048e2be5d540a7879a
ijarmar <ishanovyaro@gmail.com>
2020-01-12 15:25:33 +0200

    Added user router

--- routes/email.js
+++ routes/email.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const getTokenValues = require('../utils/verifyToken');
-const { changeEmailPassword } = require('../controllers/emailController');
+const { changeEmailPassword, createNewEmailAccount } = require('../controllers/emailController');
 
 router.post('/reset', async (request, response) => {
 	const token = request.get('Authorization').replace('Bearer ', '');
@@ -9,7 +9,7 @@ router.post('/reset', async (request, response) => {
 	const userInfo = getTokenValues(token);
 
 	if (!userInfo) {
-		response.json('Token is invalid or has expired');
+		response.json('Token is invalid or expired');
 	} else if (typeof email === 'undefined' || typeof newPass === 'undefined') {
 		response.json('Invalid values');
 	} else if (newPass.length < 8 || !/\d/.test(newPass)) {
@@ -17,6 +17,22 @@ router.post('/reset', async (request, response) => {
 	} else {
 		const message = await changeEmailPassword(userInfo, email, newPass);
 		response.json(message);
+	}
+});
+
+router.post('/add', async (request, response) => {
+	const token = request.get('Authorization').replace('Bearer', '');
+	const { email, password } = request.body;
+	const userInfo = getTokenValues(token);
+
+	if (!userInfo) {
+		response.json('Token is invalid or expired');
+	} else if (typeof email === 'undefined' || typeof newPass === 'undefined') {
+		response.json('Invalid values');
+	} else if (password.length < 8 || !/\d/.test(password)) {
+		response.json('Password must at least be 8 characters long and contain at least one digit');
+	} else {
+		await createNewEmailAccount(userInfo, email, password);
 	}
 });
 
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const getTokenValues = require('../utils/verifyToken');
+
+router.post('/', (req, res) => {});
+
+module.exports = router;
