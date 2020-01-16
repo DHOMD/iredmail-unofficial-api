e584967b2ea7918da256432f964d6c358756acf1
ijarmar <ishanovyaro@gmail.com>
2020-01-16 16:57:46 +0200

    Refactored auth.js to use async await

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const connection = require('../config/database');
+const connection = require('../config/database').promise();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const privKey = fs.readFileSync('jwtRS256.key');
@@ -10,22 +10,24 @@ router.get('/', (req, res) => {
 	res.json('Send a post request to this page for authorization');
 });
 
-router.post('/', (request, response) => {
+router.post('/', async (request, response) => {
 	const { userName, password } = request.body;
 
 	if (typeof userName === 'undefined' || typeof password === 'undefined') {
 		response.json('Invalid or missing values');
 	}
 
-	connection.query('SELECT * FROM `users` WHERE `userName` = ?', [userName], (err, res, fields) => {
-		if (res != '') {
-			bcrypt.compare(password, res[0].password, (err, result) => {
+	try {
+		const [rows] = await connection.execute('SELECT * FROM `users` WHERE `userName` = ?', [userName]);
+
+		if (rows != '') {
+			bcrypt.compare(password, rows[0].password, (err, result) => {
 				if (result) {
 					console.log('Authentication was successful');
 					jwt.sign(
 						{
 							exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
-							user: res[0].userName
+							user: rows[0].userName
 						},
 						privKey,
 						{ algorithm: 'RS256' },
@@ -44,7 +46,10 @@ router.post('/', (request, response) => {
 		} else {
 			response.json('No user found');
 		}
-	});
+	} catch (e) {
+		console.log('Failed to authenticate ' + e);
+		response.json('An error has occured, try again later');
+	}
 });
 
 module.exports = router;
--- temp.js
+++ temp.js
@@ -1,61 +1,22 @@
-// const connection = require('./config/database');
+const connection = require('./config/database').promise();
 
-// connection.changeUser({ database: 'ishanovNet' }, err => {
-// 	connection.query('SELECT `user_login` FROM `wp_users`', [], (err, res, fields) => {
-// 		console.log(res);
-// 	});
-
-// 	connection.changeUser({ database: 'controlPanel' });
-
-// 	connection.query('SELECT * FROM `users`', [], (err, res, fields) => {
-// 		console.log(
-// 			'Values in table users inside changeUser ishanov.net and after changeUser controlPanel: ',
-// 			res
-// 		);
-// 	});
-// });
-
-// connection.changeUser({ database: 'controlPanel' }, err => {
-// 	if (err) {
-// 		console.log(err);
-// 	}
-// });
-
-// connection.query('SELECT * FROM `users`', [], (err, res, fields) => {
-// 	console.log('Values in table users after switch: ', res);
-// });
+// const { exec } = require('child_process');
 
 // const hashPassword = password => {
-// 	const hash = exec(`doveadm pw -s 'ssha512' -p ${password}`, (err, stdout, stderr) => {
-// 		if (!err) {
-// 			return stdout;
-// 		}
+// 	return new Promise((resolve, reject) => {
+// 		exec(`doveadm pw -s 'ssha512' -p ${password}`, (err, stdout, stderr) => {
+// 			if (!err && !stderr) {
+// 				resolve(stdout);
+// 			}
+// 			reject('Failed to create new hash');
+// 		});
 // 	});
-// 	return hash;
 // };
+// hashPassword('1123').then(pass => console.log(pass));
 
-// hashPassword('123123');
-
-// try {
-// 	let bar = 1;
-
-// 	if (bar != 5) {
-// 		throw new Error('Bar is not 5');
-// 	}
-// 	throw new Error('This should never get thrown');
-// } catch (e) {
-// 	console.error(e.message);
-// }
-const { exec } = require('child_process');
-
-const hashPassword = password => {
-	return new Promise((resolve, reject) => {
-		exec(`doveadm pw -s 'ssha512' -p ${password}`, (err, stdout, stderr) => {
-			if (!err && !stderr) {
-				resolve(stdout);
-			}
-			reject('Failed to create new hash');
-		});
-	});
+const getRes = async () => {
+	const [rows] = await connection.execute('SELECT 1');
+	console.log(rows);
 };
-hashPassword('1123').then(pass => console.log(pass));
+
+getRes();
