f715d0b8c29573cf87c021622f18f9d3bbcc4ffc
ijarmar <ishanovyaro@gmail.com>
2020-01-18 22:03:06 +0200

    Temp

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const privKey = fs.readFileSync('jwtRS256.key');
+const pubKey = fs.readFileSync('jwt');
+
+const verifyRefreshToken = token => {
+	// jwt.verify(token, )
+};
+
+const generateAccessToken = refreshToken => {
+	const payload = verifyRefreshToken(refreshToken);
+};
+
+const generateRefreshToken = (tokenOptions, payload, signOptions) => {
+	// delete payload.json;
+	// return jwt.sign({ algorithm: 'RS256', keyid: });
+};
+
+const refreshToken = () => {};
 
 jwt.sign(
 	{
@@ -17,3 +33,8 @@ jwt.sign(
 		}
 	}
 );
+
+module.exports = {
+	generateNewToken,
+	refreshToken
+};
