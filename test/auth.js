6c6df6f55adf95e884ea912343146cf42801b024
ijarmar <ishanovyaro@gmail.com>
2020-01-21 19:28:55 +0200

    Small fixes to tests

--- test/auth.js
+++ test/auth.js
@@ -3,6 +3,8 @@ const chaiHttp = require('chai-http');
 const server = require('../src/server');
 const should = chai.should();
 const getTokenValues = require('../src/utils/verifyToken');
+const { generateRefreshToken } = require('../src/utils/generateToken');
+const { doesPasswordMatchHash } = require('../src/controllers/authController');
 
 chai.use(chaiHttp);
 
@@ -21,7 +23,7 @@ describe('/GET auth', () => {
 });
 
 describe('/POST auth', () => {
-	it('it should authenticate', done => {
+	it('it should return refreshToken', done => {
 		let obj = {
 			userName: 'info@nettifixi.fi',
 			password: 'salasana'
@@ -31,19 +33,37 @@ describe('/POST auth', () => {
 			.send(obj)
 			.end((err, res) => {
 				should.exist(res.body);
-				res.should.have.status(200);
-				res.body.should.have.property('token');
-				res.body.token.should.be.a('string');
-				res.body.token.should.not.be.empty;
-				token = res.body.token;
+				res.body.should.have.property('refreshToken');
+				res.body.refreshToken.should.be.a('string');
+				res.body.refreshToken.should.not.be.empty;
 				done();
 			});
 	});
 
+	it('password must match hash in db', done => {
+		const hash = '$2b$10$Vd/Fu65koXxTNM4dOuxW5uy242qbqY3u7ygAm12D1yTNeFvmBaUrW';
+		const password = 'salasana';
+		doesPasswordMatchHash(password, hash)
+			.then(flag => {
+				flag.should.be.a('boolean');
+				flag.should.eql(true);
+				done();
+			})
+			.catch(done);
+	});
+
+	it('it should generate token', done => {
+		token = generateRefreshToken('info@nettifix.fi');
+		token.should.be.a('string');
+		token.should.not.be.empty;
+		done();
+	});
+
 	it('token should be valid', done => {
-		getTokenValues(token).should.not.be.eql(false);
-		getTokenValues(token).should.have.property('user');
-		getTokenValues(token).should.not.be.empty;
+		const values = getTokenValues(token);
+		values.should.not.be.eql(false);
+		values.should.have.property('username');
+		values.should.not.be.empty;
 		done();
 	});
 });
