5e690321054b3f77bafc9933a60f51130db7bdc8
ijarmar <ishanovyaro@gmail.com>
2020-01-22 11:45:37 +0200

    Added refreshToken, accessToken functionalites + unit tests for them

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -4,20 +4,58 @@ const uuid = require('uuid/v4');
 const privKey = fs.readFileSync('jwtRS256.key');
 const pubKey = fs.readFileSync('jwtRS256.key.pub');
 
-const verifyRefreshToken = (token, options) => {
-	jwt.verify(token, pubKey, { audience: options.user, issuer: 'api.nettifixi.fi' });
+const isTokenValid = (token, type) => {
+	try {
+		const verified = jwt.verify(token, pubKey, {
+			issuer: 'api.nettifixi.fi',
+			algorithms: ['RS256']
+		});
+
+		if (verified && verified.type === type) {
+			return true;
+		}
+		return false;
+	} catch (e) {
+		console.log('Token could not be verified');
+		return false;
+	}
+};
+
+const getTokenValues = (token, type) => {
+	const isValid = isTokenValid(token, type);
+	if (isValid) {
+		return jwt.decode(token, { complete: true });
+	}
+	return null;
 };
 
 const generateAccessToken = refreshToken => {
-	const payload = verifyRefreshToken(refreshToken);
+	const values = getTokenValues(refreshToken, 'refreshToken');
+
+	if (values) {
+		return jwt.sign({ username: values.payload.username, type: 'accessToken' }, privKey, {
+			algorithm: 'RS256',
+			keyid: uuid(),
+			noTimestamp: false,
+			expiresIn: '15m',
+			issuer: values.payload.iss,
+			audience: values.payload.aud,
+			subject: values.payload.sub
+		});
+	}
+	return null;
 };
 
 const generateRefreshToken = username => {
+	const userArr = username.split('@');
 	const token = jwt.sign({ username, type: 'refreshToken' }, privKey, {
 		algorithm: 'RS256',
 		keyid: uuid(),
 		noTimestamp: false,
-		expiresIn: '1w'
+		expiresIn: '1w',
+		issuer: 'api.nettifixi.fi',
+		audience: userArr[1],
+		subject: userArr[0]
 	});
 
 	// think about this https://softwareengineering.stackexchange.com/questions/373109/should-we-store-jwts-in-database
@@ -27,23 +65,9 @@ const generateRefreshToken = username => {
 
 const refreshToken = () => {};
 
-jwt.sign(
-	{
-		exp: Math.floor(Date.now() / 1000) + 60 * 30 // 30 mins
-		// user: rows[0].userName
-	},
-	privKey,
-	{ algorithm: 'RS256' },
-	(err, token) => {
-		if (err) {
-			response.json('Error occured when generating token');
-		} else {
-			response.json({ token });
-		}
-	}
-);
-
 module.exports = {
-	refreshToken,
-	generateRefreshToken
+	generateRefreshToken,
+	generateAccessToken,
+	isTokenValid,
+	getTokenValues
 };
---
+++ src/utils/generateToken.test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const should = chai.should();
+const { generateRefreshToken, isTokenValid, getTokenValues, generateAccessToken } = require('./generateToken');
+
+let refreshToken;
+let accessToken;
+
+describe('testing refreshToken functionality', () => {
+	let tokenType = 'refreshToken';
+	let username = 'info@nettifixi.fi';
+	it('should generate refreshToken', done => {
+		refreshToken = generateRefreshToken(username);
+		should.exist(refreshToken);
+		refreshToken.should.be.a('string');
+		refreshToken.should.not.be.empty;
+		done();
+	});
+
+	it('token should be valid', done => {
+		const isValid = isTokenValid(refreshToken, tokenType);
+		isValid.should.be.a('boolean');
+		isValid.should.eql(true);
+		done();
+	});
+
+	it('token should store values', done => {
+		const values = getTokenValues(refreshToken, tokenType);
+		console.log(values);
+		values.should.be.an('object');
+		values.should.not.be.empty;
+		done();
+	});
+});
+
+describe('testing accessToken functionality', () => {
+	let tokenType = 'accessToken';
+	it('should generate accessToken with refreshToken', done => {
+		accessToken = generateAccessToken(refreshToken);
+		accessToken.should.be.a('string');
+		accessToken.should.not.be.empty;
+		done();
+	});
+
+	it('token should be valid', done => {
+		const isValid = isTokenValid(accessToken, tokenType);
+		isValid.should.be.a('boolean');
+		isValid.should.eql(true);
+		done();
+	});
+});
