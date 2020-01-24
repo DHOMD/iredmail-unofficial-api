f2d8459e0bd3255501c761595985689d051f020c
ijarmar <ishanovyaro@gmail.com>
2020-01-24 15:37:46 +0200

    Removed unit tests from e2e folder

--- e2e/auth.js
+++ e2e/auth.js
@@ -3,8 +3,6 @@ const chaiHttp = require('chai-http');
 const server = require('../src/server');
 const should = chai.should();
 const getTokenValues = require('../src/utils/verifyToken');
-const { generateRefreshToken } = require('../src/utils/generateToken');
-const { doesPasswordMatchHash } = require('../src/controllers/authController');
 
 chai.use(chaiHttp);
 
@@ -38,32 +36,5 @@ describe('/POST auth', () => {
 				res.body.refreshToken.should.not.be.empty;
 				done();
 			});
-	});
-
-	it('password must match hash in db', done => {
-		const hash = '$2b$10$Vd/Fu65koXxTNM4dOuxW5uy242qbqY3u7ygAm12D1yTNeFvmBaUrW';
-		const password = 'salasana';
-		doesPasswordMatchHash(password, hash)
-			.then(flag => {
-				flag.should.be.a('boolean');
-				flag.should.eql(true);
-				done();
-			})
-			.catch(done);
-	});
-
-	it('it should generate token', done => {
-		token = generateRefreshToken('info@nettifix.fi');
-		token.should.be.a('string');
-		token.should.not.be.empty;
-		done();
-	});
-
-	it('token should be valid', done => {
-		const values = getTokenValues(token);
-		values.should.not.be.eql(false);
-		values.should.have.property('username');
-		values.should.not.be.empty;
-		done();
 	});
 });
