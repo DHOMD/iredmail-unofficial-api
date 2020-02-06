2d32552a678b69364061cd60dc5469f19403af26
ijarmar <ishanovyaro@gmail.com>
2020-02-06 22:37:29 +0200

    Converted changeEmailPassword and createNewEmailAccount methods to be compatible with orm + removed old connection configuration

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const connection = require('../config/database');
+const { fn } = require('sequelize');
 const { UserDomain, User, Domain } = require('../models/controlPanel');
+const { Mailbox, Forwarding } = require('../models/vmail');
 const { exec } = require('child_process');
 const moment = require('moment');
 
@@ -36,7 +37,7 @@ const hashPassword = password => {
 	});
 };
 
-exports.changeEmailPassword = async (userInfo, email, password) => {
+const changeEmailPassword = async (userInfo, email, password) => {
 	let userMessage = '';
 	try {
 		const isAllowed = await isAllowedToModify(userInfo, email);
@@ -45,52 +46,8 @@ exports.changeEmailPassword = async (userInfo, email, password) => {
 			try {
 				const hash = await hashPassword(password);
 
-				const switchToVmailDB = new Promise((resolve, reject) => {
-					connection.changeUser(
-						{
-							user: process.env.VMAILUSER,
-							password: process.env.VMAILPASS,
-							database: 'vmail'
-						},
-						err => {
-							if (err) {
-								reject('Failed to change the database' + err);
-							}
-							resolve();
-						}
-					);
-				});
+				await Mailbox.update({ password: hash }, { where: { username: email } });
 
-				const updatePassword = new Promise((resolve, reject) => {
-					connection.execute(
-						'UPDATE `mailbox` SET `password` = ? WHERE `username` = ?',
-						[hash, email],
-						err => {
-							if (err) {
-								reject('Failed to update the password' + err);
-							}
-							resolve();
-						}
-					);
-				});
-
-				const switchToDefaultDB = new Promise((resolve, reject) => {
-					connection.changeUser(
-						{
-							database: process.env.DEFAULTDB,
-							user: process.env.DEFAULTUSER,
-							password: process.env.DEFAULTPASS
-						},
-						err => {
-							if (err) {
-								reject('Failed to change back to default database' + err);
-							}
-							resolve();
-						}
-					);
-				});
-
-				await Promise.all([switchToVmailDB, updatePassword, switchToDefaultDB]);
 				userMessage = 'Password has been successfully changed';
 			} catch (e) {
 				userMessage = 'Something went wrong on our side, try again later or contact admin';
@@ -106,7 +63,7 @@ exports.changeEmailPassword = async (userInfo, email, password) => {
 	return userMessage;
 };
 
-exports.createNewEmailAccount = async (userInfo, email, password) => {
+const createNewEmailAccount = async (userInfo, email, password) => {
 	let userMessage = '';
 	try {
 		const isAllowed = await isAllowedToModify(userInfo, email);
@@ -115,75 +72,36 @@ exports.createNewEmailAccount = async (userInfo, email, password) => {
 			try {
 				const hash = await hashPassword(password);
 
-				const switchToVmailDB = new Promise((resolve, reject) => {
-					connection.changeUser(
-						{
-							user: process.env.VMAILUSER,
-							password: process.env.VMAILPASS,
-							database: 'vmail'
-						},
-						err => {
-							if (err) {
-								reject('Failed to change the database ' + err);
-							}
-							resolve();
-						}
-					);
+				const username = email.split('@');
+				const user = username[0];
+				const domain = username[1];
+				const date = moment().format('YYYY.MM.DD.HH.mm.ss');
+				const maildir = `${domain}/${email.charAt(0)}/${email.charAt(1)}/${email.charAt(2)}/${user}-${date}/`;
+
+				const createMailbox = Mailbox.create({
+					username: email,
+					password: hash,
+					name: ' ',
+					storagebasedirectory: '/var/vmail',
+					storagenode: 'vmail1',
+					maildir: maildir,
+					quota: 0,
+					domain: domain,
+					active: 1,
+					passwordlastchange: fn('now'),
+					created: fn('now')
 				});
 
-				const createMailbox = new Promise((resolve, reject) => {
-					const username = email.split('@');
-					const user = username[0];
-					const domain = username[1];
-					const date = moment().format('YYYY.MM.DD.HH.mm.ss');
-					const maildir = `${domain}/${email.charAt(0)}/${email.charAt(1)}/${email.charAt(
-						2
-					)}/${user}-${date}/`;
-
-					connection.execute(
-						'INSERT INTO `mailbox` (username, password, name, storagebasedirectory, storagenode, maildir, quota, domain, active, passwordlastchange, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())',
-						[email, hash, '', '/var/vmail', 'vmail1', maildir, 0, domain, 1],
-						(err, res, fields) => {
-							if (err) {
-								reject('Failed to create new user ' + err);
-							}
-							resolve();
-						}
-					);
+				const createForwarding = Forwarding.create({
+					address: email,
+					forwarding: email,
+					domain: domain,
+					dest_domain: domain,
+					is_forwarding: 1
 				});
 
-				const createForwardings = new Promise((resolve, reject) => {
-					const username = email.split('@');
-					const domain = username[1];
-					connection.execute(
-						'INSERT INTO `forwardings` (address, forwarding, domain, dest_domain, is_forwarding) VALUES (?, ?, ?, ?, 1)',
-						[email, email, domain, domain],
-						(err, res, fields) => {
-							if (err) {
-								reject('Failed to insert new user into forwardings ' + err);
-							}
-							resolve();
-						}
-					);
-				});
+				await Promise.all([createMailbox, createForwarding]);
 
-				const switchToDefaultDB = new Promise((resolve, reject) => {
-					connection.changeUser(
-						{
-							database: process.env.DEFAULTDB,
-							user: process.env.DEFAULTUSER,
-							password: process.env.DEFAULTPASS
-						},
-						err => {
-							if (err) {
-								reject('Failed to change back to default database ' + err);
-							}
-							resolve();
-						}
-					);
-				});
-
-				await Promise.all([switchToVmailDB, createMailbox, createForwardings, switchToDefaultDB]);
 				userMessage = 'Successfully created new email account';
 			} catch (e) {
 				userMessage = 'Something went wrong on our side, try again later or contact admin';
@@ -199,4 +117,4 @@ exports.createNewEmailAccount = async (userInfo, email, password) => {
 	return userMessage;
 };
 
-module.exports = { isAllowedToModify, hashPassword };
+module.exports = { isAllowedToModify, hashPassword, changeEmailPassword, createNewEmailAccount };
--- src/controllers/emailController.test.js
+++ src/controllers/emailController.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 chai.should();
-const { isAllowedToModify, hashPassword } = require('./emailController');
+const { isAllowedToModify, hashPassword, changeEmailPassword, createNewEmailAccount } = require('./emailController');
 
 const userInfo = {
 	payload: {
@@ -25,6 +25,30 @@ describe('testing hashPassword', () => {
 		hashPassword('salasana1; touch file.txt')
 			.then(hash => {
 				hash.should.be.a('string');
+				done();
+			})
+			.catch(done);
+	});
+});
+
+describe('testing changeEmailPassword', () => {
+	it('should update email password', done => {
+		changeEmailPassword(userInfo, 'info@nettifixi.fi', 'salasana123')
+			.then(res => {
+				res.should.be.a('string');
+				res.should.eql('Password has been successfully changed');
+				done();
+			})
+			.catch(done);
+	});
+});
+
+describe('testing createNewEmailAccount', () => {
+	it('should create new email', done => {
+		createNewEmailAccount(userInfo, 'temp@nettifixi.fi', 'salasana123')
+			.then(res => {
+				res.should.be.a('string');
+				res.should.eql('Successfully created new email account');
 				done();
 			})
 			.catch(done);
