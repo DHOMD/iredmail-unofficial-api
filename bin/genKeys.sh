9fdece484cd8377056b148d1f1a5c535082b5a75
ijarmar <ishanovyaro@gmail.com>
2020-04-03 11:25:53 +0300

    Changed lisence to GNU as well as fixed keyGen script

--- bin/genKeys.sh
+++ bin/genKeys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh-keygen -q -N "" -t rsa -b 4096 -m PEM -f $PWD/jwtRS256test.key
+ssh-keygen -q -N "" -t rsa -b 4096 -m PEM -f $PWD/jwtRS256.key
 
-openssl rsa -in jwtRS256test.key -pubout -outform PEM -out jwtRS256test.key.pub 2>/dev/null
+openssl rsa -in jwtRS256test.key -pubout -outform PEM -out jwtRS256.key.pub 2>/dev/null
