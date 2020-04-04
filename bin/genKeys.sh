9ea15c0763bc174de5d942409df8dfe3b3e96dae
ijarmar <ishanovyaro@gmail.com>
2020-04-04 21:54:55 +0300

    Fixed genKeys.sh script and added public key to gitignore

--- bin/genKeys.sh
+++ bin/genKeys.sh
@@ -2,4 +2,4 @@
 
 ssh-keygen -q -N "" -t rsa -b 4096 -m PEM -f $PWD/jwtRS256.key
 
-openssl rsa -in jwtRS256test.key -pubout -outform PEM -out jwtRS256.key.pub 2>/dev/null
+openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub 2>/dev/null
